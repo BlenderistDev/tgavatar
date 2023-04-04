6afae7d008f48e619547c4b4856be7c7a203ef7a
BlenderistDev <demonx.blenderist@gmail.com>
2023-04-05 00:16:24 +0300

    add comments to logger

--- internal/log/log.go
+++ internal/log/log.go
@@ -2,20 +2,24 @@ package log
 
 import "github.com/sirupsen/logrus"
 
+// Logger log wrap for project
 type Logger struct {
 	log *logrus.Logger
 }
 
+// NewLogger logger constructor
 func NewLogger(log *logrus.Logger) Logger {
 	return Logger{
 		log: log,
 	}
 }
 
+// Info log with info level
 func (l Logger) Info(args ...interface{}) {
 	l.log.Infoln(args...)
 }
 
+// Error log with error level
 func (l Logger) Error(args ...interface{}) {
 	l.log.Errorln(args...)
 }
